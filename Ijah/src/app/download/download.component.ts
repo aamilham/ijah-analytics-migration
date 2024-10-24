089e95f750f0a64af26a9f3406b59c7b231267da
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-10-24 14:08:40 +0700

    Refactor HTML structure and styling in about and app components

--- Ijah/src/app/download/download.component.ts
+++ Ijah/src/app/download/download.component.ts
@@ -1,40 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.css'],
-  standalone: true, // Mark this as a standalone component
-  imports: [CommonModule, FormsModule], // Import FormsModule and CommonModule
 })
-export class DownloadComponent {
+export class DownloadComponent implements OnInit {
+  // Files array with dummy data for front-end display
   files = [
     {
       name: 'Plants_Database.csv',
       size: '2.3 MB',
       date: 'April 21, 2024',
-      link: 'downloads/Plants_Database.csv',
+      link: '#',
     },
     {
       name: 'Compounds_Database.csv',
       size: '1.7 MB',
       date: 'April 19, 2024',
-      link: 'downloads/Compounds_Database.csv',
+      link: '#',
     },
     {
       name: 'Diseases_Database.csv',
       size: '3.1 MB',
       date: 'April 18, 2024',
-      link: 'downloads/Diseases_Database.csv',
+      link: '#',
     },
   ];
 
+  // Filtered list of files to match the search
   filteredFiles = [...this.files];
 
   constructor() {}
 
+  ngOnInit(): void {}
+
+  // Function to filter files based on search input
   onSearch(event: any) {
     const searchTerm = event.target.value.toLowerCase();
     this.filteredFiles = this.files.filter((file) =>
