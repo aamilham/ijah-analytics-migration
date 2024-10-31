fc0e1ba9f77ecb9bf2eccc0cc8020dea15879412
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-10-31 14:24:16 +0700

    Refactor About and Home components for improved layout, styling, and responsiveness

--- Ijah/src/app/home/home.component.ts
+++ Ijah/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<div class="container mt-3"><h1>Welcome to the Website</h1></div>`,
+  templateUrl: './home.component.html', // The HTML file that contains the template
+  styleUrls: ['./home.component.css'], // The CSS file for the AboutComponent styles
   standalone: true,
 })
 export class HomeComponent {}
