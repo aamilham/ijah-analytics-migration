edf24e77f7888c7473141e5b6d08ac8727487141
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-11-25 13:14:37 +0700

    fix: update API endpoints with CORS support

--- Ijah/api/get_diseases.php
+++ Ijah/api/get_diseases.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'cors_header.php';
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
