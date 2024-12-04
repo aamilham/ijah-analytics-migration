503f0bbb83e45cc5f74ebe4aea76f684d26a63d5
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-12-04 21:21:31 +0700

    feat: add async animations provider to main application bootstrap

--- Ijah/src/main.ts
+++ Ijah/src/main.ts
@@ -2,10 +2,11 @@ import { routes } from './app/app.routes'; // Pastikan ini sesuai
 import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes), provideAnimationsAsync(),
     // provider lainnya
   ],
 });
