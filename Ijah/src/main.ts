2c7c79bbe93667a22b7313265855eae8144f7cde
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-12-12 23:35:03 +0700

    fix(main): add HTTP client provider to main application bootstrap

--- Ijah/src/main.ts
+++ Ijah/src/main.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), provideAnimationsAsync(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
     // provider lainnya
   ],
 });
